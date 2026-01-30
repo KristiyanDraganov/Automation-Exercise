b6b40c706a4427a92a9f0fa1ebf6bbede1560e87
KristiyanDraganov <kristiandraganov@gmail.com>
2026-01-30 22:03:10 +0200

    create logout user test

---
+++ tests/TC 4 logoutUser.spec.ts
@@ -0,0 +1,27 @@
+import {test, expect} from '@playwright/test';
+
+test('Logout user', async ({ page }) => {
+    // Navigate to the website, accept cookies and verify title
+    await page.goto('https://automationexercise.com');
+    await page.click('button[aria-label="Consent"]');
+    await expect(page).toHaveTitle('Automation Exercise');
+
+    // Click on 'Signup / Login' button and verify 'Login to your account' is visible
+    await page.click('a[href="/login"]');
+    await expect(page.getByRole('heading', { name: 'Login to your account'})).toBeVisible();
+
+    //Enter valid email address and password and click 'Login' button
+    await page.fill('input[data-qa="login-email"]', '123test@test.com');
+    await page.fill('input[data-qa="login-password"]', '123test@test.com');
+    await page.click('button[data-qa="login-button"]');
+
+    // Verify that 'Logged in as username' is visible
+    await expect(page.getByText(`Logged in as 123`)).toBeVisible();
+
+    // Click 'Logout' button
+    await page.click('a[href="/logout"]');
+
+    // Verify that user is navigated to login page
+    await expect(page).toHaveURL('https://automationexercise.com/login');
+    await expect(page.getByRole('heading', { name: 'Login to your account'})).toBeVisible();
+});
